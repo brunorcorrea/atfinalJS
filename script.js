6b5611c9cf2a3c7220029d1345a01e8d65c07394
Bruno Ricardo Corrêa <60991787+Bruno-R-Correa@users.noreply.github.com>
2020-11-13 18:21:16 -0300

    mudanças no código fonte

--- script.js
+++ script.js
@@ -10,24 +10,34 @@ function padrao(){
 function cadastrar() {
 
     document.getElementById("msg").style.color="darkred";
-    const nome = document.dados.nome;
+    const nome = document.getElementById("nome");
     const regex_nome = /[0-9]/;
 
-    if((nome.value=="") || (nome.value.length < 3) || (regex_nome.test(nome))) {
-        document.getElementById("msg").textContent="Preencha campo NOME corretamente!";
+    if((nome.value=="") || (nome.value.length < 3) || (regex_nome.test(nome.value))) {
+        document.getElementById("msg").textContent="Digite um nome válido";
         document.dados.nome.focus();
         return false;
     }
 
-    if (document.dados.idade.value < 18 ) {
+    if (document.dados.idade.value < 16 ) {
         document.getElementById("msg").textContent="Somente maiores de 16 anos podem se cadastrar!";
         document.dados.idade.focus();
         return false;
     }
 
     if((document.dados.email.value=="") || (document.dados.email.value.indexOf('@')==-1) || (document.dados.email.value.indexOf('.')==-1) ) {
-        document.getElementById("msg").textContent="Preencha campo E-MAIL corretamente!";
+        document.getElementById("msg").textContent="Insira um email válido";
         document.dados.email.focus();
+        return false;
+    }
+
+    if(document.dados.cor.value == '#4a412a'){
+        /* O motivo pelo qual não se pode selecionar essa cor(#4a412a) é porque ela é a cor mais feia do mundo:
+        https://gq.globo.com/Prazeres/Design/noticia/2016/06/cor-mais-feia-do-mundo-e-revelada-e-o-fato-vai-muito-alem-da-curiosidade.html
+        http://www.ideafixa.com/oldbutgold/cor-mais-feia-mundo
+        */
+        document.getElementById("msg").textContent="Selecione uma cor!";
+        document.dados.cor.focus();
         return false;
     }
 
